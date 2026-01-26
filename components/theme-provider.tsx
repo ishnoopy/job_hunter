857015187168e4b66fe402f19b9410af544abffa
Ruel Aldrin Guasa <114205020+ishnoopy@users.noreply.github.com>
2026-01-26 23:30:07 +0800

    chore: default theme to light

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -22,7 +22,7 @@ interface ThemeProviderProps {
 export function ThemeProvider({
   children,
   attribute = 'class',
-  defaultTheme = 'system',
+  defaultTheme = 'light',
   enableSystem = true,
   disableTransitionOnChange = false,
 }: ThemeProviderProps) {
